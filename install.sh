bc1d86020467f9c10d59b79d57b36db153d13a20
JohnathenHarker <tobiaserfurt@gmx.de>
2020-12-01 15:16:21 +0100

    changed install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # download curl
-echo downloading curl
 sudo apt install curl
+sudo apt-get install build-essential
 
 # download and copile spot
-echo downloading spot
 curl -o spot-2.9.4.tar.gz https://www.lrde.epita.fr/dload/spot/spot-2.9.4.tar.gz -fsSL
 tar -zxvf spot-2.9.4.tar.gz
 cd spot-2.9.4
 ./configure  --enable-max-accsets=256
 
-echo making spot; this may take some time
 make
 sudo make install
 sudo ldconfig
@@ -19,7 +17,6 @@ sudo ldconfig
 cd ..
 
 # run compile.sh
-echo compiling 2detTELA
 ./compile.sh
 
-echo finished installation
+echo finished installation of 2detTELA
