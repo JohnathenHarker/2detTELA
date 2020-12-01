dd0d6097f737fc45fc9722c6908b6c331122c627
JohnathenHarker <tobiaserfurt@gmx.de>
2020-12-01 13:55:01 +0100

    adding installation skript

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# download and copile spot
+echo downloading spot
+curl -o spot-2.9.4.tar.gz https://www.lrde.epita.fr/dload/spot/spot-2.9.4.tar.gz -fsSL
+tar -zxvf spot-2.9.4.tar.gz
+cd spot-2.9.4
+./configure  --enable-max-accsets=256
+
+echo making spot; this may take some time
+make
+make install
+
+cd ..
+
+# run compile.sh
+echo compiling 2detTELA
+./compile.sh
+
+echo finished installation
