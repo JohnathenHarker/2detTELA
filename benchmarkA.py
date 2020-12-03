ce28e4f1f1f755efd8b4866dc889816d1fd015e6
JohnathenHarker <tobiaserfurt@gmx.de>
2020-12-03 14:12:40 +0100

    prallelism for benchmarkA

--- benchmarkA.py
+++ benchmarkA.py
@@ -3,11 +3,17 @@ from subprocess import Popen, PIPE
 from time import sleep, perf_counter
 import csv
 import os
-
+import multiprocessing as mp
+from datetime import datetime
 
 
 # timeout for the determinisation in s
-TIMEOUT = 60
+TIMEOUT = 120
+
+# number of parallel processes
+# should be at least one core less than the number of cores of the system, to give the our sceduling process some ressources
+# here: leave two cores for the system
+CORES = mp.cpu_count()-2
 
 # path where the automata for the benchmark are stored
 
@@ -17,9 +23,12 @@ path_evaluation = "results"
 
 def popen_evaluation(command):
     # default values
-    states = -1
-    time = -1
-    acc = -1
+
+    result = {}
+    result['time'] = -1
+    result['states'] = -1
+    result['acc'] = -1
+    result['timeout'] = True
 
     p = Popen(command, stdout=PIPE, stderr=PIPE)
     tic = perf_counter()
@@ -27,20 +36,35 @@ def popen_evaluation(command):
     while (toc-tic) < TIMEOUT:
         if p.poll() is not None:
             output = p.communicate()[0].decode('UTF-8').split()
-            #print(output)
-           # print(output[0].decode('UTF-8').split())
+            result['timeout'] = False
             for i in range(len(output)):
                 if (output[i] == 'states:'):
-                    states = int(output[i+1])
+                    result['states'] = int(output[i+1])
                 if (output[i] == 'time:'):
-                    time = float(output[i+1])
+                    result['time'] = float(output[i+1])
                 if (output[i] == 'acc:'):
-                    acc = int(output[i+1])
-            return (False, states, time, acc)
+                    result['acc'] = int(output[i+1])
+            return result
         sleep(0.1)
         toc = perf_counter()
     p.kill()
-    return (True, states, time, acc)
+    return result
+
+
+def evaluate_aut(aut):
+    #print('process automaton ' + aut) # call program instead to determinise automaton
+    old_automaton = popen_evaluation(['source_code/2detTELA', '--file', aut, '--type', 'old_aut'])
+    spot = popen_evaluation(['source_code/2detTELA', '--file', aut, '--type', 'spot'])
+    product = popen_evaluation(['source_code/2detTELA', '--file', aut, '--type', 'product'])
+    return [aut,old_automaton, spot, product]
+
+
+# store the results of the evaluations
+evaluation = []
+def log_result(result):
+    evaluation.append(result)
+
+
 
 
 #### main part ###
@@ -54,11 +78,19 @@ if not os.path.exists(path_evaluation):
     os.makedirs(path_evaluation)
 
 
-
-
 ## produce examples
 
-p = Popen(['source_code/benchmarkA', '--file', path+"/automaton", '--n', '1500', '--l', '2', '--u', '15'])
+print('starting at', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+print('Generating automata')
+
+# upper and lower bound of the acc condition
+lower_bound = 2
+upper_bound = 21
+# number of generated automata
+n = 2000
+
+# call program to produce automata
+p = Popen(['source_code/benchmarkA', '--file', path+"/automaton", '--n', str(n), '--l', str(lower_bound), '--u', str(upper_bound)])
 p.wait()
 
 
@@ -66,14 +98,32 @@ p.wait()
 # extract all .hoa files from the dictionary
 automata = glob.glob(path+"/*.hoa")
 
+#sort automata names
+automata.sort()
+automata.sort(key=len)
+
+# process evaluations in parallel
+
+
+pool = mp.Pool(CORES) 
+
+print('running ', CORES , ' processes in parallel to determinise the automata')
+for aut in automata:
+    pool.apply_async(evaluate_aut, args=(aut,), callback = log_result)
+pool.close()  
+pool.join()
+
+
+print('write result into ', path_evaluation +'/benchmarkA.csv')
 ## run evaluation
 with open(path_evaluation+'/benchmarkA.csv', mode='w') as out_csv:
-
     csv_writer = csv.writer(out_csv, delimiter= ';')
-    csv_writer.writerow(['old_acc', 'timeout_spot', 'states_spot', 'time_spot', 'acc_spot', 'timeout_product', 'states_product', 'time_product', 'acc_product'])
-    for aut in automata:
-        print('process automaton ' + aut) # call program instead to determinise automaton
-        (_, _, _, old_acc) = popen_evaluation(['source_code/2detTELA', '--file', aut, '--type', 'old_aut'])
-        (timeout_s, states_s, time_s, acc_s) = popen_evaluation(['source_code/2detTELA', '--file', aut, '--type', 'spot'])
-        (timeout_p, states_p, time_p, acc_p) = popen_evaluation(['source_code/2detTELA', '--file', aut, '--type', 'product'])
-        csv_writer.writerow([old_acc, timeout_s, states_s, time_s, acc_s, timeout_p, states_p, time_p, acc_p])
+    csv_writer.writerow(['aut','old_acc', 'timeout_spot', 'states_spot', 'time_spot', 'acc_spot', 'timeout_product', 'states_product', 'time_product', 'acc_product'])
+    for row in evaluation:
+        aut = row[0]
+        old_automaton = row[1]
+        spot = row[2]
+        product = row[3]
+        csv_writer.writerow([aut, old_automaton['acc'], spot['timeout'], spot['states'], spot['time'], spot['acc'], product['timeout'], product['states'], product['time'], product['acc'],])
+
+print('finished at', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
