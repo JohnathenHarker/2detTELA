e614a92c87d5bc50e72a4bde1419f51da25fcc6a
JohnathenHarker <tobiaserfurt@gmx.de>
2020-12-08 11:13:04 +0100

    correct error in compile.sh

--- compile.sh
+++ compile.sh
@@ -3,15 +3,15 @@
 # work in source_code
 cd source_code
 # build all C++ files into object files
-g++ -I$HOME/usr/inlcude *.cc -L$HOME/usr/lib -lspot -lbddx -c
+g++ -I$HOME/usr/include *.cc -L$HOME/usr/lib -lspot -lbddx -c
 
 # compile 2detTELA
-g++ -I$HOME/usr/inlcude 2detTELA.cc my_determinization.o remfin.o -L$HOME/usr/lib -lspot -lbddx -O2 -o 2detTELA
+g++ -I$HOME/usr/include 2detTELA.cc my_determinization.o remfin.o -L$HOME/usr/lib -lspot -lbddx -O2 -o 2detTELA
 
 # compile for benchmark generations
-g++ -I$HOME/usr/inlcude benchmarkA.cc my_determinization.o remfin.o -L$HOME/usr/lib -lspot -lbddx -O2 -o benchmarkA
-g++ -I$HOME/usr/inlcude benchmarkB.cc my_determinization.o remfin.o -L$HOME/usr/lib -lspot -lbddx -O2 -o benchmarkB
-g++ -I$HOME/usr/inlcude benchmarkC.cc my_determinization.o remfin.o -L$HOME/usr/lib -lspot -lbddx -O2 -o benchmarkC
+g++ -I$HOME/usr/include benchmarkA.cc my_determinization.o remfin.o -L$HOME/usr/lib -lspot -lbddx -O2 -o benchmarkA
+g++ -I$HOME/usr/include benchmarkB.cc my_determinization.o remfin.o -L$HOME/usr/lib -lspot -lbddx -O2 -o benchmarkB
+g++ -I$HOME/usr/include benchmarkC.cc my_determinization.o remfin.o -L$HOME/usr/lib -lspot -lbddx -O2 -o benchmarkC
 
 # return to parent dictionary
 cd ..
